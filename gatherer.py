60f5de4eafa59310563e7d5d69c37c1c58964dfd
Behnam <behnam.nikbakht@gmail.com>
2022-10-19 19:28:25 -0600

    streamer

--- gatherer.py
+++ gatherer.py
@@ -57,40 +57,55 @@ def append_to_dataset(tweet, data_file):
         write_appended(data_file)
 
 
-def extract_from_tweet(item):
-    return [
-        item.author.created_at,
-        item.author.description,
-        item.author.favourites_count,
-        item.author.followers_count,
-        item.author.friends_count,
-        item.author.id,
-        item.author.location,
-        item.author.name,
-        item.created_at,
-        None if (item.entities is None or item.entities["hashtags"] is None) else ",".join(
-            [h["text"] for h in item.entities["hashtags"]]),
-        item.full_text,
-        item.id,
-        item.lang,
-        item.in_reply_to_user_id,
-        item.retweet_count,
-        item.source,
-        item.geo,
-        item.retweeted_status.id if hasattr(item, "retweeted_status") else None
-    ]
+def extract_from_tweet(item, archive=True):
+    if archive:
+        return [
+            item.author.created_at,
+            item.author.description,
+            item.author.favourites_count,
+            item.author.followers_count,
+            item.author.friends_count,
+            item.author.id,
+            item.author.location,
+            item.author.name,
+            item.created_at,
+            None if (item.entities is None or item.entities["hashtags"] is None) else ",".join(
+                [h["text"] for h in item.entities["hashtags"]]),
+            item.full_text,
+            item.id,
+            item.lang,
+            item.in_reply_to_user_id,
+            item.retweet_count,
+            item.source,
+            item.geo,
+            item.retweeted_status.id if hasattr(item, "retweeted_status") else None
+        ]
+    else:
+        return [
+            item.id,
+            item.author_id,
+            item.created_at,
+            item.lang,
+            item.source,
+            item.text
+        ]
 
 
-def retrieve(args):
+def set_path(args):
     path = args.path + "/data"
     Path(path).mkdir(parents=True, exist_ok=True)
-    data_file = path + "/dataset.csv"
+    data_file = path + "/dataset_{}.csv".format("archive" if args.archive else "stream")
     logger.info("data path = {}".format(data_file))
+    return data_file
+
+
+def archive(args):
+    q = "(#mahsa_amini OR #mahsaamini OR #مهسا_امینی) AND (-filter:retweets AND -filter:replies)"
+    data_file = set_path(args)
     last_id = args.last_id
     if last_id is None:
         last_id = sys.maxsize
     logger.info("Start with last_id = {}".format(last_id))
-    q = "(#mahsa_amini OR #mahsaamini OR #مهسا_امینی) AND (-filter:retweets AND -filter:replies)"
     i = 1
     j = 0
     api = build_api(args)
@@ -122,6 +137,35 @@ def retrieve(args):
         time.sleep(args.sleep)
 
 
+def stream(args):
+    data_file = set_path(args)
+
+    class MyStreamListener(tweepy.StreamingClient):
+        def __init__(self, bearer_token):
+            super().__init__(bearer_token)
+            self.i = 0
+        def on_tweet(self, item):
+            try:
+                tweet = extract_from_tweet(item, False)
+                append_to_dataset(tweet, data_file)
+            except Exception as ex:
+                logger.error("Error {} occurred for item {}".format(ex, item))
+            self.i += 1
+            logger.info("Stream i = {}, id = {}".format(self.i, item.id))
+
+        def on_errors(self, status_code):
+            logger.error("Error {} occurred".format(status_code))
+            return False
+
+    # stream = MyStreamListener(args.consumer_key, args.consumer_secret, args.access_token, args.access_token_secret)
+    # stream.filter(track=["#mahsa_amini", "#mahsaamini", "#مهسا_امینی"])
+    # print(stream.sample())
+
+    client = MyStreamListener(args.bearer)
+    client.add_rules([tweepy.StreamRule("#mahsa_amini OR #mahsaamini OR #مهسا_امینی")])
+    client.filter(threaded=True, tweet_fields=["author_id", "created_at","source","id","lang","text"])
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--last_id', dest='last_id', type=int, help='Last tweet ID')
@@ -139,10 +183,16 @@ if __name__ == "__main__":
     parser.add_argument('--access_token_secret', dest='access_token_secret', type=str,
                         help='API Auth access_token_secret', default=os.getenv("access_token_secret"))
     parser.add_argument('--path', dest='path', type=str, help='Output Path', default=".")
+    parser.add_argument('-archive', dest='archive', type=str, nargs="?", help='Archive', default=False, const=True)
+    parser.add_argument('--bearer', dest='bearer', type=str, help='Bearer Token', default=os.getenv("bearer"))
+
     args = parser.parse_args()
 
     setup_log(args)
 
     logger.info("args = {}".format(args))
 
-    retrieve(args)
+    if args.archive:
+        archive(args)
+    else:
+        stream(args)
